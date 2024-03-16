ac4e6c259b7dc5f2bf0cd8f3dedd68fd22a6e2e5
Saranjith <saranjith@metrictreelabs.com>
2024-03-16 07:57:02 +0530

    migration: create migration for xpence card

--- migrations/20240316022020-table_xpence_card.js
+++ migrations/20240316022020-table_xpence_card.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Users", {
+    await queryInterface.createTable("xpence_card", {
       id: {
         allowNull: false,
         primaryKey: true,
@@ -12,10 +12,16 @@ module.exports = {
       name: {
         type: Sequelize.STRING,
       },
-      email: {
+      created_month: {
         type: Sequelize.STRING,
       },
-      password: {
+      budget: {
+        type: Sequelize.STRING,
+      },
+      color: {
+        type: Sequelize.STRING,
+      },
+      is_active: {
         type: Sequelize.STRING,
       },
       createdAt: {
@@ -29,6 +35,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Users");
+    await queryInterface.dropTable("xpence_card");
   },
 };
