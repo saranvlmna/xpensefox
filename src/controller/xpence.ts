8120b22dd9335934055848b3ac57866e1c7364f3
Saranjith <saranjith@metrictreelabs.com>
2024-03-16 07:41:33 +0530

    API: createXpenceCard

--- src/controller/xpence.ts
+++ src/controller/xpence.ts
@@ -1,12 +1,87 @@
+import { StatusCodes } from "http-status-codes";
+import { XpenceService } from "../service/xpence";
+const xpenceService = new XpenceService();
 export class XpenceController {
   constructor() {}
 
-  createXpenceCard() {}
-  editXpenceCard() {}
-  deleteXpenceCard() {}
-  listXpenceCards() {}
-  fetchXpence() {}
-  addXpence() {}
-  ediXpence() {}
-  deleteXpence() {}
+  async createXpenceCard(req: any, res: any) {
+    try {
+      const response = await xpenceService.createXpenceCard();
+      res.status(StatusCodes.OK).send({
+        response,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async editXpenceCard() {
+    try {
+      const response = await xpenceService.heyServer();
+      res.status(StatusCodes.OK).send({
+        response,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async deleteXpenceCard() {
+    try {
+      const response = await xpenceService.heyServer();
+      res.status(StatusCodes.OK).send({
+        response,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async listXpenceCards() {
+    try {
+      const response = await xpenceService.heyServer();
+      res.status(StatusCodes.OK).send({
+        response,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async fetchXpence() {
+    try {
+      const response = await xpenceService.heyServer();
+      res.status(StatusCodes.OK).send({
+        response,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async addXpence() {
+    try {
+      const response = await xpenceService.heyServer();
+      res.status(StatusCodes.OK).send({
+        response,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async ediXpence() {
+    try {
+      const response = await xpenceService.heyServer();
+      res.status(StatusCodes.OK).send({
+        response,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async deleteXpence() {
+    try {
+      const response = await xpenceService.heyServer();
+      res.status(StatusCodes.OK).send({
+        response,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
